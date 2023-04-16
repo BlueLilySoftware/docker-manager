2c96d6f91aa508d6e33d372e42023a55a6cb14bb
AugustusAraujo <augustusaraujo13@gmail.com>
2023-04-16 14:47:43 -0300

    feat: add docker service

--- src/services/docker/docker.service.ts
+++ src/services/docker/docker.service.ts
@@ -1,3 +1,10 @@
-class DockerService {}
+import Dockerode from "dockerode";
+
+class DockerService {
+  constructor(private readonly dockerProvider: Dockerode) {}
+  async findAllRunningContainer() {
+    return await this.dockerProvider.listContainers();
+  }
+}
 
 export default DockerService;
