f3a4aad1d49830b0f5d8257650c33fe65818744f
fueripe-desu <fueripedesu2020@outlook.com>
2023-04-16 14:38:22 -0300

    feat: add prisma client to user service

--- src/services/users/user.service.ts
+++ src/services/users/user.service.ts
@@ -1,5 +1,14 @@
+import prisma from "../../providers/prisma/prisma.provider";
+
 class UserService {
-  createNewUser() {}
+  async createUser(email: string, passHash: string) {
+    await prisma.user.create({
+      data: {
+        email: email,
+        password: passHash,
+      },
+    });
+  }
 }
 
 export default UserService;
