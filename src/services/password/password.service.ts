6421d8031aa9fb2e832af1da74a8e3b33a80396d
fueripe-desu <fueripedesu2020@outlook.com>
2023-04-16 23:39:32 -0300

    feat: add password service

--- src/services/password/password.service.ts
+++ src/services/password/password.service.ts
@@ -1,3 +1,17 @@
-class PasswordService {}
+import bcrypt from "bcrypt";
+import userService from "../users";
+
+class PasswordService {
+  async generateHash(plain: string) {
+    const SALT_ROUNDS = 10;
+    const hash = await bcrypt.hash(plain, SALT_ROUNDS);
+    return hash;
+  }
+
+  verifyHash(candidate: string, hash: string) {
+    const match = bcrypt.compareSync(candidate, hash);
+    return match;
+  }
+}
 
 export default PasswordService;
