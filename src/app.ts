455aec5730359e42005c10e010d6d25275dc81f3
fueripe-desu <fueripedesu2020@outlook.com>
2023-04-16 23:42:13 -0300

    feat: add passport middleware and user router to the main express server

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,18 @@ import express from "express";
 // import { PrismaClient } from "@prisma/client";
 import { indexRouter } from "./routes";
 import dockerRouter from "./routes/docker.route";
+import userRouter from "./routes/users.route";
+import passport from "./providers/passport/passport.provider";
 
 // export const prisma = new PrismaClient();
 
+// TODO: Needs to setup Postgress database to finish auth system
+// TODO: Needs to setup DATABASE_URL variable in the .env file
+
 export const app = express();
 
+app.use(passport.initialize());
+
 app.use("/", indexRouter);
-app.use('/docker', dockerRouter)
+app.use("/docker", dockerRouter);
+app.use("/user", userRouter);
